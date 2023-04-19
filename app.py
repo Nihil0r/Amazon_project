ad58a8f18c10080d4a3ceb7eb71143983f1cefb0
nihil0r <brice.grivet.1995@gmail.com>
2023-04-19 21:34:17 +0200

    étape problème flask_couchdb

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+print(sys.executable)
+
 from flask import Flask, render_template
 from flask_couchdb import CouchDBManager
 
@@ -12,7 +15,12 @@ couchdb.setup(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    categories = [
+        {"id": 1, "name": "Electronics"},
+        {"id": 2, "name": "Books"},
+        # Add more categories as needed
+    ]
+    return render_template('home.html', title="Amazon Project - Accueil", categories=categories)
 
 if __name__ == '__main__':
     app.run(debug=True)
